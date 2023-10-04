213b5848f51cd47f30fbc0465801bae13c86bea8
Akiff-hplusstree <akiff@htree.plus>
2023-10-04 13:01:25 +0000

    Added rest of the files

---
+++ test.py
@@ -0,0 +1,17 @@
+def check_number(str):
+    for i in range(len(str)):
+        if str[i].isdigit() == False:
+            return False
+    return True
+
+str = "Akiff"
+if check_number(str):
+    print(str + " is an integer ")
+else:
+    print(str + " is a string ")
+
+str1 = "123"
+if check_number(str1):
+    print(str1 + " is an integer ")
+else:
+    print(str1 + " is a string ")
