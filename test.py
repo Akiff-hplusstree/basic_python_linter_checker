17d4f1340526f2b1220973dbe849df23a3d90280
Akiff-hplusstree <akiff@htree.plus>
2023-10-04 13:19:18 +0000

    test corrected according to PEP8

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 def check_number(str):
     for i in range(len(str)):
-        if str[i].isdigit() == False:
+        if str[i].isdigit() is False:
             return False
     return True
+
 
 str = "Akiff"
 if check_number(str):
     print(str + " is an integer ")
 else:
     print(str + " is a string ")
+
 
 str1 = "123"
 if check_number(str1):
