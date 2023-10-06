a47377cb009709090c7a9f0525a5009bbf573fba
Akiff Khoja <akiff@htree.plus>
2023-10-06 09:20:31 +0000

    PEP8 according lint check

---
+++ check_akiff.py
@@ -0,0 +1,24 @@
+def add_numbers(x, y):
+result = x + y
+return result
+
+
+def subtract_numbers(x, y):
+    result = x - y
+    return result
+
+
+def multiply_numbers(x, y):
+    result = x * y
+    return result
+
+
+def divide_numbers(x, y):
+    result = x / y
+    return result
+
+
+print("Addition: ", add_numbers(5, 3))
+print("Subtraction: ", subtract_numbers(10, 4))
+print("Multiplication: ", multiply_numbers(7, 2))
+print("Division: ", divide_numbers(8, 2))
--- test.py
+++ test.py
@@ -4,13 +4,11 @@ def check_number(str):
             return False
     return True
 
-
 str = "Akiff"
 if check_number(str):
     print(str + " is an integer ")
 else:
     print(str + " is a string ")
-
 
 str1 = "123"
 if check_number(str1):
